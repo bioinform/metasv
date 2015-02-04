6bc42682d71eb77f73044a261cf7ce81677fccb6
John Mu <johnmu@umnhoj.com>
2015-02-04 12:40:12 -0800

    comments and formatting

--- metasv/main.py
+++ metasv/main.py
@@ -274,13 +274,15 @@ def run_metasv(sample, reference, pindel_vcf=[], pindel_native=[], breakdancer_v
             if vcf_record is not None:
                 key = (interval.sv_type, "PASS" if interval.is_validated else "LowQual",
                        "PRECISE" if interval.is_precise else "IMPRECISE", tuple(sorted(list(interval.sources))))
-                if key not in final_stats: final_stats[key] = 0
+                if key not in final_stats:
+                    final_stats[key] = 0
                 final_stats[key] += 1
                 vcf_writer.write_record(vcf_record)
             bed_interval = interval.to_bed_interval(sample)
             if bed_interval is not None:
                 bed_intervals.append(bed_interval)
 
+    # Also save a BED file representation of the merged variants without assembly
     pybedtools.BedTool(bed_intervals).saveas(merged_bed)
     vcf_fd.close()
     vcf_writer.close()
@@ -290,6 +292,7 @@ def run_metasv(sample, reference, pindel_vcf=[], pindel_native=[], breakdancer_v
 
     final_vcf = os.path.join(outdir, "variants.vcf")
 
+    # Run assembly here
     if not disable_assembly:
         logger.info("Running assembly")
         if spades is None:
@@ -307,11 +310,12 @@ def run_metasv(sample, reference, pindel_vcf=[], pindel_native=[], breakdancer_v
 
         assembly_bed = merged_bed
 
+        # this does the improved assembly location finder with softclipped reads
         if boost_ins:
             logger.info("Generating intervals for insertions")
             assembly_bed = parallel_generate_sc_intervals([bam.name], list(contig_whitelist), merged_bed, workdir,
                                                           num_threads=num_threads)
-            logger.info("Generated intervals for assembly in %s" % (assembly_bed))
+            logger.info("Generated intervals for assembly in %s" % assembly_bed)
 
         logger.info("Will run assembly now")
 
