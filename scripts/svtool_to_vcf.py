97720e1ec19029487c161236a41da75d3d6c524a
Marghoob Mohiyuddin <marghoobm@binatechnologies.com>
2015-02-03 17:34:21 -0800

    Add support for sorting

--- scripts/svtool_to_vcf.py
+++ scripts/svtool_to_vcf.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 tool_to_reader = {"BreakDancer": BreakDancerReader, "Pindel": PindelReader, "CNVnator": CNVnatorReader, "BreakSeq": BreakSeqReader}
 
 
-def convert_svtool_to_vcf(file_name, sample, out_vcf, toolname, reference):
+def convert_svtool_to_vcf(file_name, sample, out_vcf, toolname, reference, sort=False):
     vcf_template_reader = get_template()
     vcf_template_reader.samples = [sample]
 
@@ -25,16 +25,24 @@ def convert_svtool_to_vcf(file_name, sample, out_vcf, toolname, reference):
     vcf_writer = vcf.Writer(vcf_fd, vcf_template_reader)
 
     reference_handle = pysam.Fastafile(reference) if reference else None
+    vcf_records = []
     for tool_record in tool_to_reader[toolname](file_name, reference_handle = reference_handle):
         vcf_record = tool_record.to_vcf_record(sample)
         if vcf_record is None:
             continue
+        if sort:
+            vcf_records.append(vcf_record)
+        else:
+            vcf_writer.write_record(vcf_record)
+
+    vcf_records.sort(cmp=lambda x, y: cmp((x.CHROM, x.POS), (y.CHROM, y.POS)))
+    for vcf_record in vcf_records:
         vcf_writer.write_record(vcf_record)
     vcf_writer.close()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Convert SV tool output file to VCF",
+    parser = argparse.ArgumentParser(description="Convert SV tool output file to VCF",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--input", help="SV tool output file", required=False)
     parser.add_argument("--output", help="Output VCF to create", required=False)
@@ -42,6 +50,7 @@ if __name__ == "__main__":
                         choices=["BreakDancer", "CNVnator", "Pindel"])
     parser.add_argument("--sample", help="Sample name", required=True)
     parser.add_argument("--reference", help = "Reference FASTA")
+    parser.add_argument("--sort", action = "store_true", help = "Sort the VCF records before writing")
 
     args = parser.parse_args()
-    convert_svtool_to_vcf(args.input, args.sample, args.output, args.tool, args.reference)
+    convert_svtool_to_vcf(args.input, args.sample, args.output, args.tool, args.reference, sort=args.sort)
