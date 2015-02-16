6fcdf801f8f3b8da750b07157006982e864f56d0
Marghoob Mohiyuddin <marghoobm@binatechnologies.com>
2015-02-16 09:53:14 -0800

    Handle missing fields

--- metasv/vcf_utils.py
+++ metasv/vcf_utils.py
@@ -113,6 +113,10 @@ def load_intervals(in_vcf, intervals={}, gap_intervals=[], include_intervals=[],
             if source == "BreakSeq" and "PASS" not in vcf_record.FILTER: continue
 
             if len(vcf_record.ALT) > 1: continue
+            if "SVTYPE" not in vcf_record.INFO or "END" not in vcf_record.INFO:
+                logger.error("Missing SVTYPE or INFO field in %s" % (str(vcf_record)))
+                continue
+
             sv_type = vcf_record.INFO["SVTYPE"]
             if sv_type == "DUP:TANDEM": sv_type = "DUP"
             if "SVLEN" not in vcf_record.INFO:
