7ff4d4858ef0902b9d6c5a3c450bf484793ba9fa
Marghoob Mohiyuddin <marghoobm@binatechnologies.com>
2015-07-01 17:39:01 -0700

    formatting

--- scripts/run_metasv.py
+++ scripts/run_metasv.py
@@ -6,7 +6,6 @@ from metasv.main import run_metasv
 from metasv.defaults import *
 from metasv._version import __version__
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Merge SVs from multiple tools for accurate SV calling",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -14,33 +13,39 @@ if __name__ == "__main__":
     input_parser = parser.add_argument_group("Input data options")
     input_parser.add_argument("--sample", metavar="Sample", help="Sample name", required=True)
     input_parser.add_argument("--pindel_vcf", nargs="+", metavar="pindel_vcf", help="VCF file or dir for Pindel VCFs",
-                        required=False, default=[])
-    input_parser.add_argument("--pindel_native", nargs="+", metavar="File list", help="Pindel native files", required=False,
-                        default=[])
+                              required=False, default=[])
+    input_parser.add_argument("--pindel_native", nargs="+", metavar="File list", help="Pindel native files",
+                              required=False,
+                              default=[])
     input_parser.add_argument("--breakdancer_vcf", nargs="+", metavar="breakdancer_vcf",
-                        help="VCF file or dir for BreakDancer VCFs", required=False, default=[])
+                              help="VCF file or dir for BreakDancer VCFs", required=False, default=[])
     input_parser.add_argument("--breakdancer_native", nargs="+", metavar="File list", help="BreakDancer native files",
-                        required=False, default=[])
-    input_parser.add_argument("--breakseq_vcf", nargs="+", metavar="breakseq_vcf", help="VCF file or dir for BreakSeq VCFs",
-                        required=False, default=[])
-    input_parser.add_argument("--breakseq_native", nargs="+", metavar="breakseq_native", help="BreakSeq native GFF files",
-                        required=False, default=[])
-    input_parser.add_argument("--cnvnator_vcf", nargs="+", metavar="cnvnator_vcf", help="VCF file or dir for CNVnator VCFs",
-                        required=False, default=[])
+                              required=False, default=[])
+    input_parser.add_argument("--breakseq_vcf", nargs="+", metavar="breakseq_vcf",
+                              help="VCF file or dir for BreakSeq VCFs",
+                              required=False, default=[])
+    input_parser.add_argument("--breakseq_native", nargs="+", metavar="breakseq_native",
+                              help="BreakSeq native GFF files",
+                              required=False, default=[])
+    input_parser.add_argument("--cnvnator_vcf", nargs="+", metavar="cnvnator_vcf",
+                              help="VCF file or dir for CNVnator VCFs",
+                              required=False, default=[])
     input_parser.add_argument("--cnvnator_native", nargs="+", metavar="File list", help="CNVnator native files",
-                        required=False,
-                        default=[])
-    input_parser.add_argument("--gatk_vcf", nargs="+", metavar="file", help="VCF file or dir for gatk VCFs", required=False,
-                        default=[])
+                              required=False,
+                              default=[])
+    input_parser.add_argument("--gatk_vcf", nargs="+", metavar="file", help="VCF file or dir for gatk VCFs",
+                              required=False,
+                              default=[])
 
     reference_parser = parser.add_argument_group("Reference options")
     reference_parser.add_argument("--reference", metavar="reference", help="Reference file", required=True)
     reference_parser.add_argument("--chromosomes",
-                        help="Chromosome list to process. If unspecified, then all chromosomes will be considered.",
-                        nargs="+", default=[])
+                                  help="Chromosome list to process. If unspecified, then all chromosomes will be considered.",
+                                  nargs="+", default=[])
     reference_parser.add_argument("--gaps", metavar="gaps", help="Gap bed file", required=False, default=None)
     reference_parser.add_argument("--filter_gaps", help="Filter out gaps", action="store_true", required=False)
-    reference_parser.add_argument("--keep_standard_contigs", action="store_true", help="Keep only the major contigs + MT")
+    reference_parser.add_argument("--keep_standard_contigs", action="store_true",
+                                  help="Keep only the major contigs + MT")
 
     bam_parser = parser.add_argument_group("Input BAM options")
     bam_parser.add_argument("--bam", help="BAM", type=file)
@@ -48,40 +53,48 @@ if __name__ == "__main__":
     bam_parser.add_argument("--isize_sd", type=float, default=ISIZE_SD, help="Insert size standard deviation")
 
     merging_parser = parser.add_argument_group("Tool output merging options")
-    merging_parser.add_argument("--wiggle", help="Wiggle for interval overlap", default=WIGGLE, type=int, required=False)
-    merging_parser.add_argument("--inswiggle", help="Wiggle for insertions, overides wiggle", default=INS_WIGGLE, type=int,
-                        required=False)
-    merging_parser.add_argument("--minsvlen", help="Minimum length acceptable to be an SV", default=MIN_SV_LENGTH, type=int,
-                        required=False)
+    merging_parser.add_argument("--wiggle", help="Wiggle for interval overlap", default=WIGGLE, type=int,
+                                required=False)
+    merging_parser.add_argument("--inswiggle", help="Wiggle for insertions, overides wiggle", default=INS_WIGGLE,
+                                type=int,
+                                required=False)
+    merging_parser.add_argument("--minsvlen", help="Minimum length acceptable to be an SV", default=MIN_SV_LENGTH,
+                                type=int,
+                                required=False)
     merging_parser.add_argument("--overlap_ratio", help="Reciprocal overlap ratio", default=OVERLAP_RATIO, type=float,
-                        required=False)
+                                required=False)
 
     insertion_parser = parser.add_argument_group("Insertion detection options")
-    insertion_parser.add_argument("--boost_ins", help="Use soft-clips for improving insertion detection", action="store_true")
-    insertion_parser.add_argument("--min_ins_support", help="Minimum read support for calling insertions using soft-clips",
-                        type=int, default=MIN_SUPPORT)
+    insertion_parser.add_argument("--boost_ins", help="Use soft-clips for improving insertion detection",
+                                  action="store_true")
+    insertion_parser.add_argument("--min_ins_support",
+                                  help="Minimum read support for calling insertions using soft-clips",
+                                  type=int, default=MIN_SUPPORT)
     insertion_parser.add_argument("--min_ins_support_frac",
-                        help="Minimum fraction of reads supporting insertion using soft-clips", type=float,
-                        default=MIN_SUPPORT_FRAC)
-    insertion_parser.add_argument("--max_ins_intervals", help="Maximum number of insertion intervals to generate", type=int,
-                        default=MAX_INTERVALS)
+                                  help="Minimum fraction of reads supporting insertion using soft-clips", type=float,
+                                  default=MIN_SUPPORT_FRAC)
+    insertion_parser.add_argument("--max_ins_intervals", help="Maximum number of insertion intervals to generate",
+                                  type=int,
+                                  default=MAX_INTERVALS)
 
     as_parser = parser.add_argument_group("Assembly options")
     as_parser.add_argument("--spades", help="Path to SPAdes executable", required=False)
     as_parser.add_argument("--disable_assembly", action="store_true", help="Disable assembly")
     as_parser.add_argument("--disable_deletion_assembly", action="store_true", help="Disable assembly for deletions")
-    as_parser.add_argument("--extraction_max_read_pairs", type=int, default=EXTRACTION_MAX_READ_PAIRS, help="Maximum number of pairs to extract for assembly")
+    as_parser.add_argument("--extraction_max_read_pairs", type=int, default=EXTRACTION_MAX_READ_PAIRS,
+                           help="Maximum number of pairs to extract for assembly")
     as_parser.add_argument("--stop_spades_on_fail", action="store_true", help="Abort on SPAdes failure")
     as_parser.add_argument("--age", help="Path to AGE executable", required=False)
 
     gt_parser = parser.add_argument_group("Genotyping options")
     gt_parser.add_argument("--gt_window", type=int, default=GT_WINDOW, help="Window for genotyping")
-    gt_parser.add_argument("--gt_normal_frac", type=float, default=GT_NORMAL_FRAC, help="Min. fraction of reads supporting reference for genotyping")
+    gt_parser.add_argument("--gt_normal_frac", type=float, default=GT_NORMAL_FRAC,
+                           help="Min. fraction of reads supporting reference for genotyping")
 
     out_parser = parser.add_argument_group("Output options")
     out_parser.add_argument("--svs_to_report", nargs="+", help="SV types to report", default=SVS_SUPPORTED)
     out_parser.add_argument("--enable_per_tool_output", action="store_true",
-                        help="Enable output of merged SVs for individual tools")
+                            help="Enable output of merged SVs for individual tools")
 
     work_parser = parser.add_argument_group("Running environment options")
     work_parser.add_argument("--workdir", help="Scratch directory for working", default="work", required=False)
@@ -106,5 +119,6 @@ if __name__ == "__main__":
                         enable_per_tool_output=args.enable_per_tool_output, min_support=args.min_ins_support,
                         min_support_frac=args.min_ins_support_frac, max_intervals=args.max_ins_intervals,
                         disable_deletion_assembly=args.disable_deletion_assembly,
-                        stop_spades_on_fail=args.stop_spades_on_fail, gt_window=args.gt_window, gt_normal_frac=args.gt_normal_frac, isize_mean=args.isize_mean, isize_sd=args.isize_sd, extraction_max_read_pairs=args.extraction_max_read_pairs, svs_to_report=args.svs_to_report))
-
+                        stop_spades_on_fail=args.stop_spades_on_fail, gt_window=args.gt_window,
+                        gt_normal_frac=args.gt_normal_frac, isize_mean=args.isize_mean, isize_sd=args.isize_sd,
+                        extraction_max_read_pairs=args.extraction_max_read_pairs, svs_to_report=args.svs_to_report))
